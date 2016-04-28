8b50317dc6526db7a8d22acf481ff6e1e9eb80c1
Xubuntu-64 <giovane.preczeski@hp.com>
2016-04-28 11:38:32 -0700

    Testando 2 pull

--- teste.py
+++ teste.py
@@ -1,8 +1,14 @@
 from github import Github
+import redis
 
-g=Github("c3f3c3b1b432d0496c9e3305ba647121da44156f");
+serverRedis = redis.Redis("localhost")
+pullRequests= []
+githubAccount=Github("09124264d954adbb6462fa0fe8ae2399ddd9cca7")
 #repositories = list(g.get_repos())
-for repo in g.get_user().get_repos():
-    if(repo.name == "teste"):
-    	repositorie = repo
-print repo.
+for repositorie in githubAccount.get_user().get_repos():
+    for pull in repositorie.get_pulls(): 
+    		if(pull.state == "open"):
+    			pullRequests.append(pull)
+    			serverRedis.sadd("Pull_Request","title:"+pull.title+",state:"+pull.state+",created_at:"+pull.created_at.strftime("%m/%d/%Y")+",repositorie:"+repositorie.name+",User:"+pull.user.name)			
+print serverRedis.smembers("Pull_Request")	
+	
